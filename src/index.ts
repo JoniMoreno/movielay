2bfc91567d4a96a2bbd0e4be02eb03eff272ace1
Yehonatan Moreno <jonimoreno2004@gmail.com>
2023-11-16 22:53:46 +0200

    Adding middlewares and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello Typescript!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import pino from "pino/";
+
+const logger = pino({});
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  logger.info(`Listenning on port ${process.env.PORT}`);
+});
